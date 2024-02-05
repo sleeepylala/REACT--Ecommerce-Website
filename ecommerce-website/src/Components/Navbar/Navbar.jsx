1d96745624be7f778b8fc22686349285cf50c974
ilaria <ilariaferrauti@gmail.com>
2024-02-05 15:11:13 +0100

    ora aggiungo le route dentro la navbar

--- ecommerce-website/src/Components/Navbar/Navbar.jsx
+++ ecommerce-website/src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   //creazione state per switchare l'hr in base a quale li clicco
@@ -18,33 +19,37 @@ const Navbar = () => {
             setMenu("shop");
           }}
         >
-          Shop {menu === "shop" ? <hr /> : <></>}
+          <Link to="/">Shop</Link> {menu === "shop" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("mens");
           }}
         >
-          Men {menu === "mens" ? <hr /> : <></>}
+          <Link to="/mens">Men</Link> {menu === "mens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("womens");
           }}
         >
-          Women {menu === "womens" ? <hr /> : <></>}
+          <Link to="/womens">Women</Link> {menu === "womens" ? <hr /> : <></>}
         </li>
         <li
           onClick={() => {
             setMenu("kids");
           }}
         >
-          Kids {menu === "kids" ? <hr /> : <></>}
+          <Link to="/kids">Kids</Link> {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="cart-icon" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="cart-icon" />
+        </Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
