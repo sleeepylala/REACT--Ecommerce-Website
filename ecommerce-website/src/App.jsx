19bc8bf9a72f030863d7626348c5c74d7c2689aa
ilaria <ilariaferrauti@gmail.com>
2024-02-05 15:01:17 +0100

    ora setto le route per i prodotti e per gli id dei prodotti

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
 //Con browserRouter i componenti saranno disponibili in ogni pagina
 
 function App() {
@@ -13,9 +14,12 @@ function App() {
       </BrowserRouter>
       <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategory />} />
-        <Route path="/womens" element={<ShopCategory />} />
-        <Route path="/kids" element={<ShopCategory />} />
+        <Route path="/mens" element={<ShopCategory category="men" />} />
+        <Route path="/womens" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kid" />} />
+
+        <Route path="/product" element={<Product />} />
+        <Route path=":productId" element={<Product />} />
       </Routes>
     </div>
   );
