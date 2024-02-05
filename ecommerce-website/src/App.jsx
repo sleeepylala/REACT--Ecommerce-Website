b0d4d006363c8d1e0c9ada93dc5cf9c19b8be3dd
ilaria <ilariaferrauti@gmail.com>
2024-02-05 15:04:49 +0100

    poi creo la route per le restanti  pagine (cart, login)

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 //Con browserRouter i componenti saranno disponibili in ogni pagina
 
 function App() {
@@ -20,6 +22,10 @@ function App() {
 
         <Route path="/product" element={<Product />} />
         <Route path=":productId" element={<Product />} />
+
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/login" element={<LoginSignup />} />
       </Routes>
     </div>
   );
