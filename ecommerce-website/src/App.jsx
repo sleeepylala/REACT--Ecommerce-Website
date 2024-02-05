a0afe4a1a8ae3a566779945b5dfcfe838281af1a
ilaria <ilariaferrauti@gmail.com>
2024-02-05 18:53:54 +0100

    creo footer component e lo stile

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -6,6 +6,7 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
 //Con browserRouter i componenti saranno disponibili in ogni pagina
 
 function App() {
@@ -27,6 +28,7 @@ function App() {
 
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
