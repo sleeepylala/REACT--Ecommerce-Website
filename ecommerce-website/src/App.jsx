61d4528fdc0a4cfd7d111ed8d5a60639648fa34e
ilaria <ilariaferrauti@gmail.com>
2024-02-05 14:54:39 +0100

    aggiungo le route delle varie pages

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+//Con browserRouter i componenti saranno disponibili in ogni pagina
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory />} />
+        <Route path="/womens" element={<ShopCategory />} />
+        <Route path="/kids" element={<ShopCategory />} />
+      </Routes>
     </div>
   );
 }
