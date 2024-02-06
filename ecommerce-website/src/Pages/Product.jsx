f69ccad3cf32a34decd38a606a71e5c8e5c3f004
ilaria <ilariaferrauti@gmail.com>
2024-02-06 14:45:43 +0100

    aggiungo il componente dentro il componente Product.jsx

--- ecommerce-website/src/Pages/Product.jsx
+++ ecommerce-website/src/Pages/Product.jsx
@@ -2,12 +2,17 @@ import React, { useContext } from "react";
 import "./CSS/Product.css";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
   const { productId } = useParams(); //prendo il parametro da dentro App.jsx, così posso trovare l'id di tutti i prodotti
   const product = all_product.find((e) => e.id === Number(productId)); //Viene utilizzata la funzione find sull'array all_product per trovare il prodotto il cui ID corrisponde all'ID estratto dalla URL. L'ID viene convertito in un numero
-  return <div></div>;
+  return (
+    <div>
+      <Breadcrum product={product} />
+    </div>
+  );
 };
 
 export default Product;
