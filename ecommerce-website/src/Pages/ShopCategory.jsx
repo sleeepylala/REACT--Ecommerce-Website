1f05735bb3111dde9d63920042040c058e7c6060
ilaria <ilariaferrauti@gmail.com>
2024-02-06 12:33:16 +0100

    useContext per prendere i valori e utilizzarli da altre parti

--- ecommerce-website/src/Pages/ShopCategory.jsx
+++ ecommerce-website/src/Pages/ShopCategory.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import "./CSS/ShopCategory.css";
 
-const ShopCategory = () => {
-  return <div></div>;
+//questo componente ShopCategory utilizza il contesto ShopContext per ottenere accesso ai dati sui prodotti
+const ShopCategory = (props) => {
+  const { all_product } = useContext(ShopContext); //stiamo utilizzando il hook useContext di React per accedere al contesto ShopContext
+  return <div className="shop-category"></div>;
 };
 
 export default ShopCategory;
