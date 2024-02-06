71b8f02a2ac27335d1a1f2970f1e2197f0e59e60
ilaria <ilariaferrauti@gmail.com>
2024-02-06 13:03:05 +0100

    nelle shopcategory mostro solo il componente item che matcha con la categoria

--- ecommerce-website/src/Pages/ShopCategory.jsx
+++ ecommerce-website/src/Pages/ShopCategory.jsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import "./CSS/ShopCategory.css";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Item/Item";
 
 //questo componente ShopCategory utilizza il contesto ShopContext per ottenere accesso ai dati sui prodotti
 const ShopCategory = (props) => {
@@ -8,6 +10,32 @@ const ShopCategory = (props) => {
   return (
     <div className="shop-category">
       <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
   );
 };
