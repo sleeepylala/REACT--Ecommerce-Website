5de2d7f1578fa0137076dff467d5547d39bfcade
ilaria <ilariaferrauti@gmail.com>
2024-02-06 11:16:55 +0100

    createcontext in react nel componente ShopContext
    createContext è una funzione di React che viene utilizzata per creare un contesto, ovvero un modo per passare dati attraverso l'albero dei componenti senza dover passare esplicitamente le props manualmente ad ogni livello.

---
+++ ecommerce-website/src/Context/ShopContext.jsx
@@ -0,0 +1,27 @@
+// Import di React e del metodo createContext da React
+import React, { createContext } from "react";
+
+// Import del modulo all_product contenente le informazioni sui prodotti
+import all_product from "../Components/Assets/all_product";
+
+// Creazione di un contesto chiamato ShopContext utilizzando la funzione createContext di React
+export const ShopContext = createContext(null);
+
+// Definizione del componente ShopContextProvider
+const ShopContextProvider = (props) => {
+  // Creazione di un oggetto contextValue che contiene tutte le informazioni sui prodotti
+  const contextValue = { all_product };
+
+  // Restituzione del Provider del contesto ShopContext con il valore contextValue
+  return (
+    // Il Provider del contesto ShopContext fornisce il valore contextValue a tutti i componenti figli
+    // In questo modo, i componenti figli possono accedere al valore del contesto
+    <ShopContext.Provider value={contextValue}>
+      {/* Rendere disponibili i componenti figli */}
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
+
+// Esportazione del componente ShopContextProvider come default
+export default ShopContextProvider;
