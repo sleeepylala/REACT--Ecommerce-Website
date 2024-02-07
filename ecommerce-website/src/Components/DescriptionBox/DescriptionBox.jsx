c68a66b1225687b316231f34cd362f29512ea782
ilaria <ilariaferrauti@gmail.com>
2024-02-07 13:52:50 +0100

    creazione e stile del componente DescriptionBox

---
+++ ecommerce-website/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci
+          cumque vel unde, cupiditate aliquid maxime earum deserunt
+          reprehenderit dolore et fugit, libero iure, velit eius eligendi
+          nesciunt inventore sequi sed. Lorem ipsum dolor sit amet consectetur,
+          adipisicing elit. Architecto reiciendis voluptatem vel esse, corporis
+          distinctio eius vero voluptatibus, dignissimos molestias, blanditiis
+          perspiciatis alias excepturi. Iure error adipisci doloribus odit
+          delectus.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Reprehenderit quae recusandae eius modi voluptatibus
+          autem quos magnam nobis nesciunt incidunt nisi magni, doloribus
+          corporis suscipit libero nam earum velit ex!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
